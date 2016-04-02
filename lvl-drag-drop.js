fbb2297db717c742868843228f6f152f7eda78fc
Fredrik Löfgren <frelo223@student.liu.se>
2016-04-02 23:19:01 +0000

    Modal popup on right click

--- lvl-drag-drop.js
+++ lvl-drag-drop.js
@@ -80,6 +80,22 @@ app.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, tile) {
 });
 
 
+
+
+app.directive('ngRightClick', function($parse) {
+    return function(scope, element, attrs) {
+        var fn = $parse(attrs.ngRightClick);
+        element.bind('contextmenu', function(event) {
+            scope.$apply(function() {
+                event.preventDefault();
+                fn(scope, {$event:event});
+            });
+        });
+    };
+});
+
+
+
 app.directive('tile', function() {
     return {
         scope: {
