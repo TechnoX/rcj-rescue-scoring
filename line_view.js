72d1630c48af23c40f84d695693cf1ab059621d3
Fredrik Löfgren <frelo223@student.liu.se>
2016-04-03 19:22:53 +0000

    Support for different viewing styles

--- line_view.js
+++ line_view.js
@@ -15,12 +15,14 @@ app.controller('ddController', ['$scope', '$uibModal', '$log', function($scope, 
         return arr;
     }
 
+    $scope.visType = "slider";
+
     $scope.z = 1;
     $scope.tiles = {};
 
-    $scope.tiles["2,3,1"] = {rot: 180, image: 'tiles/tile-5.png', dropTile: false,
+    $scope.tiles["2,3,1"] = {rot: 180, image: 'tiles/tile-5.png', dropTile: true,
                              items: {gaps: 0, obstacles: 0, speedbumps: 0, intersections: 0},
-                             scored: {gaps: [], obstacles: [], speedbumps: [], intersections: [], reach: [false]}};
+                             scored: {gaps: [], obstacles: [], speedbumps: [], intersections: [], reach: [true]}};
     $scope.tiles["3,3,1"] = {rot: 90, image: 'tiles/tile-5.png', dropTile: false,
                              items: {gaps: 0, obstacles: 0, speedbumps: 0, intersections: 1},
                              scored: {gaps: [], obstacles: [], speedbumps: [], intersections: [false]}};
@@ -28,6 +30,19 @@ app.controller('ddController', ['$scope', '$uibModal', '$log', function($scope, 
                              items: {gaps: 0, obstacles: 1, speedbumps: 0, intersections: 0},
                              scored: {gaps: [], obstacles: [false], speedbumps: [], intersections: []}};
     $scope.tiles["2,2,1"] = {rot: 270, image: 'tiles/tile-5.png', dropTile: false,
+                             items: {gaps: 0, obstacles: 0, speedbumps: 0, intersections: 0},
+                             scored: {gaps: [], obstacles: [], speedbumps: [], intersections: [], reach: [false,false]}};
+
+    $scope.tiles["2,4,2"] = {rot: 180, image: 'tiles/tile-6.png', dropTile: true,
+                             items: {gaps: 0, obstacles: 0, speedbumps: 0, intersections: 0},
+                             scored: {gaps: [], obstacles: [], speedbumps: [], intersections: [], reach: [true]}};
+    $scope.tiles["3,4,2"] = {rot: 90, image: 'tiles/tile-6.png', dropTile: false,
+                             items: {gaps: 0, obstacles: 0, speedbumps: 0, intersections: 0},
+                             scored: {gaps: [], obstacles: [], speedbumps: [], intersections: []}};
+    $scope.tiles["3,3,2"] = {rot: 0, image: 'tiles/tile-6.png', dropTile: false,
+                             items: {gaps: 0, obstacles: 0, speedbumps: 0, intersections: 0},
+                             scored: {gaps: [], obstacles: [], speedbumps: [], intersections: []}};
+    $scope.tiles["2,3,2"] = {rot: 270, image: 'tiles/tile-6.png', dropTile: false,
                              items: {gaps: 0, obstacles: 0, speedbumps: 0, intersections: 0},
                              scored: {gaps: [], obstacles: [], speedbumps: [], intersections: [], reach: [false,false]}};
 
