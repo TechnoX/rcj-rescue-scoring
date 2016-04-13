6fafea3d63fececc93135e89f7618e307e313c31
Sebastian Gustafsson <sebgu182@student.liu.se>
2016-04-13 20:58:12 +0200

    Added id to frontend routes

--- routes/home.js
+++ routes/home.js
@@ -10,14 +10,14 @@ router.get('/', function (req, res) {
 router.get('/line/', function (req, res) {
   res.render('index');
 })
-router.get('/line/editor', function (req, res) {
-  res.render('line_editor');
+router.get('/line/editor/:mapid', function (req, res) {
+  res.render('line_editor', {id : req.params.mapid})
 })
-router.get('/line/judge', function (req, res) {
-  res.render('line_judge');
+router.get('/line/judge/:roundid', function (req, res) {
+  res.render('line_judge', {id : req.params.roundid})
 })
-router.get('/line/view', function (req, res) {
-  res.render('line_view');
+router.get('/line/view/:roundid', function (req, res) {
+  res.render('line_view', {id : req.params.roundid})
 })
 
 
