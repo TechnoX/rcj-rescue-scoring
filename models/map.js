4423976a5ba6d905186a28621d1bc88ddc89fded
Sebastian Gustafsson <sebgu182@student.liu.se>
2016-04-15 20:03:59 +0200

    Added hard-coded tiletypes

--- models/map.js
+++ models/map.js
@@ -61,47 +61,328 @@ var TileType = mongoose.model('TileType', tileTypeSchema)
 module.exports.map = Map
 module.exports.tileType = TileType
 
-var testMap = new Map({
-  name             : "Test1",
-  height           : 1,
-  width            : 2,
-  length           : 1,
-  tiles            : [
-    {
-      x       : 0,
-      y       : 0,
-      z       : 0,
-      rot     : 0,
-      tileType: "5708b8ff54deddcd27756aa3",
-      items   : {
-        obstacles : 2,
-        speedbumps: 1
-      }
+var tileTypes = [
+  {
+    "image": "tile-0.png",
+    "gaps": 0,
+    "intersections": 0,
+    "paths": {
+      "left": "right",
+      "right": "left"
     },
-    {
-      x       : 1,
-      y       : 0,
-      z       : 0,
-      rot     : 0,
-      tileType: "5708b8ff54deddcd27756aa3",
-      items   : {
-        obstacles : 2,
-        speedbumps: 0
-      }
-    }
-  ],
-  startTile        : {x: 0, y: 0, z: 0},
-  numberOfDropTiles: 1
-})
+    "_id": "570c27c3f5a9dabe23f3af90"
+  },
+  {
+    "image": "tile-1.png",
+    "gaps": 0,
+    "intersections": 0,
+    "paths": {
+      "left": "right",
+      "right": "left"
+    },
+    "_id": "570c27c3f5a9dabe23f3af91"
+  },
+  {
+    "image": "tile-2.png",
+    "gaps": 0,
+    "intersections": 0,
+    "paths": {
+      "left": "right",
+      "right": "left"
+    },
+    "_id": "570c27c3f5a9dabe23f3af92"
+  },
+  {
+    "image": "tile-3.png",
+    "gaps": 0,
+    "intersections": 0,
+    "paths": {
+      "left": "bottom",
+      "bottom": "left"
+    },
+    "_id": "570c27c3f5a9dabe23f3af93"
+  },
+  {
+    "image": "tile-4.png",
+    "gaps": 0,
+    "intersections": 0,
+    "paths": {
+      "left": "top",
+      "top": "left",
+      "right": "bottom",
+      "bottom": "right"
+    },
+    "_id": "570c27c3f5a9dabe23f3af94"
+  },
+  {
+    "image": "tile-5.png",
+    "gaps": 0,
+    "intersections": 0,
+    "paths": {
+      "left": "bottom",
+      "bottom": "left"
+    },
+    "_id": "570c27c3f5a9dabe23f3af95"
+  },
+  {
+    "image": "tile-6.png",
+    "gaps": 0,
+    "intersections": 0,
+    "paths": {
+      "left": "bottom",
+      "bottom": "left"
+    },
+    "_id": "570c27c3f5a9dabe23f3af96"
+  },
+  {
+    "image": "tile-7.png",
+    "gaps": 0,
+    "intersections": 0,
+    "paths": {
+      "top": "bottom",
+      "bottom": "top"
+    },
+    "_id": "570c27c3f5a9dabe23f3af97"
+  },
+  {
+    "image": "tile-8.png",
+    "gaps": 0,
+    "intersections": 0,
+    "paths": {
+      "left": "right",
+      "right": "left"
+    },
+    "_id": "570c27c3f5a9dabe23f3af98"
+  },
+  {
+    "image": "tile-9.png",
+    "gaps": 0,
+    "intersections": 0,
+    "paths": {
+      "left": "right",
+      "right": "left"
+    },
+    "_id": "570c27c3f5a9dabe23f3af99"
+  },
+  {
+    "image": "tile-10.png",
+    "gaps": 0,
+    "intersections": 0,
+    "paths": {
+      "left": "bottom",
+      "bottom": "left"
+    },
+    "_id": "570c27c3f5a9dabe23f3af9a"
+  },
+  {
+    "image": "tile-11.png",
+    "gaps": 2,
+    "intersections": 0,
+    "paths": {
+      "left": "right",
+      "right": "left"
+    },
+    "_id": "570c27c3f5a9dabe23f3af9b"
+  },
+  {
+    "image": "tile-12.png",
+    "gaps": 0,
+    "intersections": 0,
+    "paths": {
+      "right": "bottom",
+      "bottom": "right"
+    },
+    "_id": "570c27c3f5a9dabe23f3af9c"
+  },
+  {
+    "image": "tile-13.png",
+    "gaps": 0,
+    "intersections": 0,
+    "paths": {
+      "left": "right",
+      "right": "left"
+    },
+    "_id": "570c27c3f5a9dabe23f3af9d"
+  },
+  {
+    "image": "tile-14.png",
+    "gaps": 0,
+    "intersections": 0,
+    "paths": {
+      "left": "right",
+      "right": "left"
+    },
+    "_id": "570c27c3f5a9dabe23f3af9e"
+  },
+  {
+    "image": "tile-15.png",
+    "gaps": 0,
+    "intersections": 1,
+    "paths": {
+      "left": "top",
+      "top": "left",
+      "right": "left"
+    },
+    "_id": "570c27c3f5a9dabe23f3af9f"
+  },
+  {
+    "image": "tile-16.png",
+    "gaps": 0,
+    "intersections": 1,
+    "paths": {
+      "left": "right",
+      "right": "top",
+      "top": "right"
+    },
+    "_id": "570c27c3f5a9dabe23f3afa0"
+  },
+  {
+    "image": "tile-17.png",
+    "gaps": 0,
+    "intersections": 0,
+    "paths": {
+      "left": "right",
+      "right": "left"
+    },
+    "_id": "570c27c3f5a9dabe23f3afa1"
+  },
+  {
+    "image": "tile-18.png",
+    "gaps": 0,
+    "intersections": 0,
+    "paths": {
+      "left": "right",
+      "right": "left"
+    },
+    "_id": "570c27c3f5a9dabe23f3afa2"
+  },
+  {
+    "image": "tile-19.png",
+    "gaps": 1,
+    "intersections": 0,
+    "paths": {
+      "left": "right",
+      "right": "left",
+      "top": "bottom",
+      "bottom": "top"
+    },
+    "_id": "570c27c3f5a9dabe23f3afa3"
+  },
+  {
+    "image": "tile-20.png",
+    "gaps": 1,
+    "intersections": 0,
+    "paths": {
+      "left": "right",
+      "right": "left"
+    },
+    "_id": "570c27c3f5a9dabe23f3afa4"
+  },
+  {
+    "image": "tile-21.png",
+    "gaps": 0,
+    "intersections": 0,
+    "paths": {
+      "bottom": "right",
+      "right": "bottom"
+    },
+    "_id": "570c27c3f5a9dabe23f3afa5"
+  },
+  {
+    "image": "tile-22.png",
+    "gaps": 0,
+    "intersections": 0,
+    "paths": {
+      "bottom": "right",
+      "right": "bottom"
+    },
+    "_id": "570c27c3f5a9dabe23f3afa6"
+  },
+  {
+    "image": "tile-23.png",
+    "gaps": 0,
+    "intersections": 0,
+    "paths": {
+      "left": "right",
+      "right": "left"
+    },
+    "_id": "570c27c3f5a9dabe23f3afa7"
+  },
+  {
+    "image": "tile-24.png",
+    "gaps": 0,
+    "intersections": 0,
+    "paths": {
+      "bottom": "right",
+      "right": "bottom"
+    },
+    "_id": "570c27c3f5a9dabe23f3afa8"
+  },
+  {
+    "image": "tile-25.png",
+    "gaps": 0,
+    "intersections": 0,
+    "paths": {
+      "left": "top",
+      "top": "left",
+      "bottom": "right",
+      "right": "bottom"
+    },
+    "_id": "570c27c3f5a9dabe23f3afa9"
+  },
+  {
+    "image": "tile-26.png",
+    "gaps": 0,
+    "intersections": 0,
+    "paths": {
+      "bottom": "right",
+      "right": "bottom"
+    },
+    "_id": "570c27c3f5a9dabe23f3afaa"
+  },
+  {
+    "image": "tile-27.png",
+    "gaps": 0,
+    "intersections": 1,
+    "paths": {
+      "bottom": "right",
+      "right": "bottom"
+    },
+    "_id": "570c27c3f5a9dabe23f3afab"
+  },
+  {
+    "image": "tile-28.png",
+    "gaps": 0,
+    "intersections": 1,
+    "paths": {
+      "left": "right",
+      "right": "left"
+    },
+    "_id": "570c27c3f5a9dabe23f3afac"
+  },
+  {
+    "image": "tile-29.png",
+    "gaps": 0,
+    "intersections": 1,
+    "paths": {
+      "left": "bottom",
+      "bottom": "left"
+    },
+    "_id": "570c27c3f5a9dabe23f3afad"
+  }
+]
 
-testMap.save(function (err, data) {
-  if (err) {
-    console.log(err);
-  }
-  else {
-    console.log("saved map");
-  }
-})
+for (i in tileTypes) {
+  var tileType = new TileType(tileTypes[i])
+  tileType.save(function (err, data) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      console.log("saved tiletype");
+    }
+  })
+}
 
 pathFinder.findPath({
     name     : "Test2",
