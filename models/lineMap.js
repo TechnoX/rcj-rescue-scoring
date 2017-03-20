2afe8300b6a59b402fff4036ca1466777acd03c5
Sebastian Gustafsson <sebgu182@student.liu.se>
2017-03-20 14:04:04 +0100

    Added dead-end tiletypes

--- models/lineMap.js
+++ models/lineMap.js
@@ -139,396 +139,395 @@ module.exports.tileType = TileType
 
 const tileTypes = [
   {
-    "image"        : "tile-0.png",
-    "gaps"         : 0,
-    "intersections": 0,
-    "paths"        : {
+    "image"       : "tile-0.png",
+    "gaps"        : 0,
+    "intersection": false,
+    "paths"       : {
       "left" : "right",
       "right": "left"
     },
-    "_id"          : "570c27c3f5a9dabe23f3af90"
+    "_id"         : "570c27c3f5a9dabe23f3af90"
   },
   {
-    "image"        : "tile-1.png",
-    "gaps"         : 0,
-    "intersections": 0,
-    "paths"        : {
+    "image"       : "tile-1.png",
+    "gaps"        : 0,
+    "intersection": false,
+    "paths"       : {
       "left" : "right",
       "right": "left"
     },
-    "_id"          : "570c27c3f5a9dabe23f3af91"
+    "_id"         : "570c27c3f5a9dabe23f3af91"
   },
   {
-    "image"        : "tile-2.png",
-    "gaps"         : 0,
-    "intersections": 0,
-    "paths"        : {
+    "image"       : "tile-2.png",
+    "gaps"        : 0,
+    "intersection": false,
+    "paths"       : {
       "left" : "right",
       "right": "left"
     },
-    "_id"          : "570c27c3f5a9dabe23f3af92"
+    "_id"         : "570c27c3f5a9dabe23f3af92"
   },
   {
-    "image"        : "tile-3.png",
-    "gaps"         : 0,
-    "intersections": 0,
-    "paths"        : {
+    "image"       : "tile-3.png",
+    "gaps"        : 0,
+    "intersection": false,
+    "paths"       : {
       "left"  : "bottom",
       "bottom": "left"
     },
-    "_id"          : "570c27c3f5a9dabe23f3af93"
+    "_id"         : "570c27c3f5a9dabe23f3af93"
   },
   {
-    "image"        : "tile-4.png",
-    "gaps"         : 0,
-    "intersections": 0,
-    "paths"        : {
+    "image"       : "tile-4.png",
+    "gaps"        : 0,
+    "intersection": false,
+    "paths"       : {
       "left"  : "top",
       "top"   : "left",
       "right" : "bottom",
       "bottom": "right"
     },
-    "_id"          : "570c27c3f5a9dabe23f3af94"
+    "_id"         : "570c27c3f5a9dabe23f3af94"
   },
   {
-    "image"        : "tile-5.png",
-    "gaps"         : 0,
-    "intersections": 0,
-    "paths"        : {
+    "image"       : "tile-5.png",
+    "gaps"        : 0,
+    "intersection": false,
+    "paths"       : {
       "left"  : "bottom",
       "bottom": "left"
     },
-    "_id"          : "570c27c3f5a9dabe23f3af95"
+    "_id"         : "570c27c3f5a9dabe23f3af95"
   },
   {
-    "image"        : "tile-6.png",
-    "gaps"         : 0,
-    "intersections": 0,
-    "paths"        : {
+    "image"       : "tile-6.png",
+    "gaps"        : 0,
+    "intersection": false,
+    "paths"       : {
       "left"  : "bottom",
       "bottom": "left"
     },
-    "_id"          : "570c27c3f5a9dabe23f3af96"
+    "_id"         : "570c27c3f5a9dabe23f3af96"
   },
   {
-    "image"        : "tile-7.png",
-    "gaps"         : 0,
-    "intersections": 0,
-    "paths"        : {
+    "image"       : "tile-7.png",
+    "gaps"        : 0,
+    "intersection": false,
+    "paths"       : {
       "top"   : "bottom",
       "bottom": "top"
     },
-    "_id"          : "570c27c3f5a9dabe23f3af97"
+    "_id"         : "570c27c3f5a9dabe23f3af97"
   },
   {
-    "image"        : "tile-8.png",
-    "gaps"         : 0,
-    "intersections": 0,
-    "paths"        : {
+    "image"       : "tile-8.png",
+    "gaps"        : 0,
+    "intersection": false,
+    "paths"       : {
       "left" : "right",
       "right": "left"
     },
-    "_id"          : "570c27c3f5a9dabe23f3af98"
+    "_id"         : "570c27c3f5a9dabe23f3af98"
   },
   {
-    "image"        : "tile-9.png",
-    "gaps"         : 0,
-    "intersections": 0,
-    "paths"        : {
+    "image"       : "tile-9.png",
+    "gaps"        : 0,
+    "intersection": false,
+    "paths"       : {
       "left" : "right",
       "right": "left"
     },
-    "_id"          : "570c27c3f5a9dabe23f3af99"
+    "_id"         : "570c27c3f5a9dabe23f3af99"
   },
   {
-    "image"        : "tile-10.png",
-    "gaps"         : 0,
-    "intersections": 0,
-    "paths"        : {
+    "image"       : "tile-10.png",
+    "gaps"        : 0,
+    "intersection": false,
+    "paths"       : {
       "left"  : "bottom",
       "bottom": "left"
     },
-    "_id"          : "570c27c3f5a9dabe23f3af9a"
+    "_id"         : "570c27c3f5a9dabe23f3af9a"
   },
   {
-    "image"        : "tile-11.png",
-    "gaps"         : 2,
-    "intersections": 0,
-    "paths"        : {
+    "image"       : "tile-11.png",
+    "gaps"        : 2,
+    "intersection": false,
+    "paths"       : {
       "left" : "right",
       "right": "left"
     },
-    "_id"          : "570c27c3f5a9dabe23f3af9b"
+    "_id"         : "570c27c3f5a9dabe23f3af9b"
   },
   {
-    "image"        : "tile-12.png",
-    "gaps"         : 0,
-    "intersections": 0,
-    "paths"        : {
+    "image"       : "tile-12.png",
+    "gaps"        : 0,
+    "intersection": false,
+    "paths"       : {
       "right" : "bottom",
       "bottom": "right"
     },
-    "_id"          : "570c27c3f5a9dabe23f3af9c"
+    "_id"         : "570c27c3f5a9dabe23f3af9c"
   },
   {
-    "image"        : "tile-13.png",
-    "gaps"         : 0,
-    "intersections": 0,
-    "paths"        : {
+    "image"       : "tile-13.png",
+    "gaps"        : 0,
+    "intersection": false,
+    "paths"       : {
       "left" : "right",
       "right": "left"
     },
-    "_id"          : "570c27c3f5a9dabe23f3af9d"
+    "_id"         : "570c27c3f5a9dabe23f3af9d"
   },
   {
-    "image"        : "tile-14.png",
-    "gaps"         : 0,
-    "intersections": 0,
-    "paths"        : {
+    "image"       : "tile-14.png",
+    "gaps"        : 0,
+    "intersection": false,
+    "paths"       : {
       "left" : "right",
       "right": "left"
     },
-    "_id"          : "570c27c3f5a9dabe23f3af9e"
+    "_id"         : "570c27c3f5a9dabe23f3af9e"
   },
   {
-    "image"        : "tile-15.png",
-    "gaps"         : 0,
-    "intersections": 1,
-    "paths"        : {
+    "image"       : "tile-15.png",
+    "gaps"        : 0,
+    "intersection": true,
+    "paths"       : {
       "left" : "top",
       "top"  : "left",
       "right": "left"
     },
-    "_id"          : "570c27c3f5a9dabe23f3af9f"
+    "_id"         : "570c27c3f5a9dabe23f3af9f"
   },
   {
-    "image"        : "tile-16.png",
-    "gaps"         : 0,
-    "intersections": 1,
-    "paths"        : {
+    "image"       : "tile-16.png",
+    "gaps"        : 0,
+    "intersection": true,
+    "paths"       : {
       "left" : "right",
       "right": "top",
       "top"  : "right"
     },
-    "_id"          : "570c27c3f5a9dabe23f3afa0"
+    "_id"         : "570c27c3f5a9dabe23f3afa0"
   },
   {
-    "image"        : "tile-17.png",
-    "gaps"         : 0,
-    "intersections": 0,
-    "paths"        : {
+    "image"       : "tile-16_2.png",
+    "gaps"        : 0,
+    "intersection": true,
+    "paths"       : {
+      "left" : "top",
+      "right": "top",
+      "top"  : "top"
+    },
+    "_id"         : "58cfd29b204466244ba56a1f"
+  },
+  {
+    "image"       : "tile-17.png",
+    "gaps"        : 0,
+    "intersection": false,
+    "paths"       : {
       "left" : "right",
       "right": "left"
     },
-    "_id"          : "570c27c3f5a9dabe23f3afa1"
+    "_id"         : "570c27c3f5a9dabe23f3afa1"
   },
   {
-    "image"        : "tile-18.png",
-    "gaps"         : 0,
-    "intersections": 0,
-    "paths"        : {
+    "image"       : "tile-18.png",
+    "gaps"        : 0,
+    "intersection": false,
+    "paths"       : {
       "left" : "right",
       "right": "left"
     },
-    "_id"          : "570c27c3f5a9dabe23f3afa2"
+    "_id"         : "570c27c3f5a9dabe23f3afa2"
   },
   {
-    "image"        : "tile-19.png",
-    "gaps"         : 1,
-    "intersections": 0,
-    "paths"        : {
+    "image"       : "tile-19.png",
+    "gaps"        : 1,
+    "intersection": false,
+    "paths"       : {
       "left"  : "right",
       "right" : "left",
       "top"   : "bottom",
       "bottom": "top"
     },
-    "_id"          : "570c27c3f5a9dabe23f3afa3"
+    "_id"         : "570c27c3f5a9dabe23f3afa3"
   },
   {
-    "image"        : "tile-20.png",
-    "gaps"         : 1,
-    "intersections": 0,
-    "paths"        : {
+    "image"       : "tile-20.png",
+    "gaps"        : 1,
+    "intersection": false,
+    "paths"       : {
       "left" : "right",
       "right": "left"
     },
-    "_id"          : "570c27c3f5a9dabe23f3afa4"
+    "_id"         : "570c27c3f5a9dabe23f3afa4"
   },
   {
-    "image"        : "tile-21.png",
-    "gaps"         : 0,
-    "intersections": 0,
-    "paths"        : {
+    "image"       : "tile-21.png",
+    "gaps"        : 0,
+    "intersection": false,
+    "paths"       : {
       "bottom": "right",
       "right" : "bottom"
     },
-    "_id"          : "570c27c3f5a9dabe23f3afa5"
+    "_id"         : "570c27c3f5a9dabe23f3afa5"
   },
   {
-    "image"        : "tile-22.png",
-    "gaps"         : 0,
-    "intersections": 0,
-    "paths"        : {
+    "image"       : "tile-22.png",
+    "gaps"        : 0,
+    "intersection": false,
+    "paths"       : {
       "bottom": "right",
       "right" : "bottom"
     },
-    "_id"          : "570c27c3f5a9dabe23f3afa6"
+    "_id"         : "570c27c3f5a9dabe23f3afa6"
   },
   {
-    "image"        : "tile-23.png",
-    "gaps"         : 0,
-    "intersections": 0,
-    "paths"        : {
+    "image"       : "tile-23.png",
+    "gaps"        : 0,
+    "intersection": false,
+    "paths"       : {
       "left" : "right",
       "right": "left"
     },
-    "_id"          : "570c27c3f5a9dabe23f3afa7"
+    "_id"         : "570c27c3f5a9dabe23f3afa7"
   },
   {
-    "image"        : "tile-24.png",
-    "gaps"         : 0,
-    "intersections": 0,
-    "paths"        : {
+    "image"       : "tile-24.png",
+    "gaps"        : 0,
+    "intersection": false,
+    "paths"       : {
       "bottom": "right",
       "right" : "bottom"
     },
-    "_id"          : "570c27c3f5a9dabe23f3afa8"
+    "_id"         : "570c27c3f5a9dabe23f3afa8"
   },
   {
-    "image"        : "tile-25.png",
-    "gaps"         : 0,
-    "intersections": 0,
-    "paths"        : {
+    "image"       : "tile-25.png",
+    "gaps"        : 0,
+    "intersection": false,
+    "paths"       : {
       "left"  : "top",
       "top"   : "left",
       "bottom": "right",
       "right" : "bottom"
     },
-    "_id"          : "570c27c3f5a9dabe23f3afa9"
+    "_id"         : "570c27c3f5a9dabe23f3afa9"
   },
   {
-    "image"        : "tile-26.png",
-    "gaps"         : 0,
-    "intersections": 0,
-    "paths"        : {
+    "image"       : "tile-26.png",
+    "gaps"        : 0,
+    "intersection": false,
+    "paths"       : {
       "bottom": "right",
       "right" : "bottom"
     },
-    "_id"          : "570c27c3f5a9dabe23f3afaa"
+    "_id"         : "570c27c3f5a9dabe23f3afaa"
   },
   {
-    "image"        : "tile-27.png",
-    "gaps"         : 0,
-    "intersections": 1,
-    "paths"        : {
+    "image"       : "tile-27.png",
+    "gaps"        : 0,
+    "intersection": true,
+    "paths"       : {
       "bottom": "right",
       "right" : "bottom"
     },
-    "_id"          : "570c27c3f5a9dabe23f3afab"
+    "_id"         : "570c27c3f5a9dabe23f3afab"
   },
   {
-    "image"        : "tile-28.png",
-    "gaps"         : 0,
-    "intersections": 1,
-    "paths"        : {
+    "image"       : "tile-28.png",
+    "gaps"        : 1,
+    "intersection": true,
+    "paths"       : {
       "left" : "right",
       "right": "left"
     },
-    "_id"          : "570c27c3f5a9dabe23f3afac"
+    "_id"         : "570c27c3f5a9dabe23f3afac"
   },
   {
-    "image"        : "tile-29.png",
-    "gaps"         : 0,
-    "intersections": 1,
-    "paths"        : {
+    "image"       : "tile-29.png",
+    "gaps"        : 0,
+    "intersection": true,
+    "paths"       : {
       "left"  : "bottom",
       "bottom": "left"
     },
-    "_id"          : "570c27c3f5a9dabe23f3afad"
+    "_id"         : "570c27c3f5a9dabe23f3afad"
   },
   {
-    "image"        : "tile-30.png",
-    "gaps"         : 0,
-    "intersections": 1,
-    "paths"        : {
+    "image"       : "tile-30.png",
+    "gaps"        : 0,
+    "intersection": true,
+    "paths"       : {
       "left"  : "right",
       "bottom": "top",
       "right" : "top",
       "top"   : "right"
     },
-    "_id"          : "570c27c3f5a9dabe23f3afae"
+    "_id"         : "570c27c3f5a9dabe23f3afae"
   },
   {
-    "image"        : "tile-31.png",
-    "gaps"         : 0,
-    "intersections": 1,
-    "paths"        : {
-      "left"  : "right",
-      "bottom": "right",
-      "right" : "bottom",
-      "top"   : "bottom"
-    },
-    "_id"          : "570c27c3f5a9dabe23f3afaf"
-  },
-  {
-    "image"        : "tile-32.png",
-    "gaps"         : 0,
-    "intersections": 1,
-    "paths"        : {
-      "left"  : "bottom",
-      "bottom": "top",
-      "right" : "left",
-      "top"   : "bottom"
-    },
-    "_id"          : "570c27c3f5a9dabe23f3afb0"
-  },
-  {
-    "image"        : "tile-33.png",
-    "gaps"         : 0,
-    "intersections": 1,
-    "paths"        : {
+    "image"       : "tile-31.png",
+    "gaps"        : 0,
+    "intersection": true,
+    "paths"       : {
       "left"  : "top",
-      "top"   : "left",
-      "right" : "left",
-      "bottom": "top"
+      "bottom": "top",
+      "right" : "top",
+      "top"   : "top"
     },
-    "_id"          : "570c27c3f5a9dabe23f3afb1"
+    "_id"         : "58cfd29b204466244ba56a20"
   },
   {
-    "image"        : "tile-34.png",
-    "gaps"         : 0,
-    "intersections": 1,
-    "paths"        : {
+    "image"       : "tile-32.png",
+    "gaps"        : 0,
+    "intersection": true,
+    "paths"       : {
       "left"  : "top",
       "top"   : "left",
       "right" : "bottom",
       "bottom": "right"
     },
-    "_id"          : "570c27c3f5a9dabe23f3afb2"
+    "_id"         : "570c27c3f5a9dabe23f3afb2"
   },
   {
-    "image"        : "tile-35.png",
-    "gaps"         : 0,
-    "intersections": 1,
-    "paths"        : {
-      "left"  : "bottom",
-      "top"   : "right",
-      "right" : "top",
-      "bottom": "left"
+    "image"       : "tile-33.png",
+    "gaps"        : 0,
+    "intersection": true,
+    "paths"       : {
+      "left"  : "left",
+      "bottom": "bottom",
+      "right" : "bottom",
+      "top"   : "left"
     },
-    "_id"          : "570c27c3f5a9dabe23f3afb3"
+    "_id"         : "58cfd29b204466244ba56a21"
   },
   {
-    "image"        : "tile-36.png",
-    "gaps"         : 0,
-    "intersections": 1,
-    "paths"        : {
+    "image"       : "tile-34.png",
+    "gaps"        : 0,
+    "intersection": true,
+    "paths"       : {
+      "left"  : "left",
+      "bottom": "bottom",
+      "right" : "right",
+      "top"   : "top"
+    },
+    "_id"         : "58cfd29b204466244ba56a22"
+  },
+  {
+    "image"       : "tile-35.png",
+    "gaps"        : 0,
+    "intersection": true,
+    "paths"       : {
       "left"  : "right",
       "top"   : "bottom",
       "right" : "left",
       "bottom": "top"
     },
-    "_id"          : "570c27c3f5a9dabe23f3afb4"
+    "_id"         : "570c27c3f5a9dabe23f3afb4"
   }
 ]
 
