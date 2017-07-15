8db742b354ab6f6180ab4c8c69347d23928cf120
TechnoX <fredrik@eaproduktion.se>
2017-07-15 16:25:57 +0200

    Updated maze judge run view to be similar to line

--- public/javascripts/maze_competition.js
+++ public/javascripts/maze_competition.js
@@ -27,4 +27,53 @@ angular.module("MazeCompetition", []).controller("MazeCompetitionController", fu
             "/maze/maps").then(function (response) {
     $scope.maps = response.data
   })
+
+  $scope.go = function(path){
+      window.location = path
+  }
+  
+  $scope.go_judge = function(path){
+      swal({
+          title: "Judge?", 
+          text: "Are you sure to move Judge Page?", 
+          type: "warning",
+          showCancelButton: true,
+          confirmButtonText: "Yes",
+        }, function() {
+          $scope.go(path);
+      }, function (error) {
+        console.log(error);
+    });
+  }
+  
+  $scope.no_judge = function(){
+      swal("Oops!", "The run you selected was already ended! If you need to edit, please contact OC.", "error");
+  }
 })
+.directive("runsReadFinished", function($timeout){
+    return function(scope, element, attrs){
+      if (scope.$last){
+        $('.refine').css("visibility","visible");
+        $('.loader').remove();
+        $timeout(function(){
+          $('table.comp').exTableFilter({
+            filters : {
+                0 : {
+                    append : {
+                        to : 'div.filter-round',
+                        type : 'checkbox'
+                    }
+                },
+                2 : {
+                    append : {
+                        to : 'div.filter-arena',
+                        type : 'checkbox'
+                    }
+                }
+            }
+          });
+        });
+      }
+    }
+})
+
