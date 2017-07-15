df832ae40955f46c7947703070b7b00fc4d2cc6c
TechnoX <fredrik@eaproduktion.se>
2017-07-15 17:11:52 +0200

    Does not work yet. Can't call updateRunList since it is not a function

--- public/javascripts/maze_run_admin.js
+++ public/javascripts/maze_run_admin.js
@@ -1,8 +1,17 @@
-angular.module("RunAdmin", []).controller("RunAdminController", function ($scope, $http) {
+var socket;
+angular.module("RunAdmin", ['ngAnimate']).controller('RunAdminController', ['$scope', '$http', '$log','$location', function($scope, $http, $log, $location){
   $scope.competitionId = competitionId
 
   updateRunList()
 
+  (function launchSocketIo() {
+        // launch socket.io
+            socket = io.connect(window.location.origin);
+            socket.emit('subscribe', 'competition/' + competitionId);
+            socket.on('changed', function(data) {
+                updateRunList();
+            });
+  })();
   $http.get("/api/competitions/" + competitionId).then(function (response) {
     $scope.competition = response.data
   })
@@ -55,25 +64,91 @@ angular.module("RunAdmin", []).controller("RunAdminController", function ($scope
       updateRunList()
     }, function (error) {
       console.log(error)
+      swal("Oops!", error.data.err, "error");
     })
   }
 
   $scope.removeRun = function (run) {
-    if (confirm("Are you sure you want to remove the run: " + '?')) {
+    swal({
+          title: "Delete Run?", 
+          text: "Are you sure you want to remove the run?", 
+          type: "warning",
+          showCancelButton: true,
+          confirmButtonText: "Yes, delete it!",
+          confirmButtonColor: "#ec6c62"
+        }, function() {
       $http.delete("/api/runs/maze/" + run._id).then(function (response) {
         console.log(response)
         updateRunList()
       }, function (error) {
         console.log(error)
       })
-    }
+        });
   }
 
   function updateRunList() {
     $http.get("/api/competitions/" + competitionId +
               "/maze/runs?populate=true").then(function (response) {
       $scope.runs = response.data
-      console.log($scope.teams)
-    })
+              })
   }
+    $scope.go_sign = function(runid){
+        swal({
+          title: "Go sign page?", 
+          text: "Are you sure you want to go sign page? If you click 'GO', the signatures will remove.", 
+          type: "warning",
+          showCancelButton: true,
+          confirmButtonText: "GO!",
+          confirmButtonColor: "#ec6c62"
+        }, function() {
+            $scope.go('/maze/sign/'+runid+'/');
+         });
+    }
+    
+    $scope.go_judge = function(runid){
+        swal({
+          title: "Go judge page?", 
+          text: "Are you sure you want to go judge page? If you click 'GO', the time and signatures will remove.", 
+          type: "warning",
+          showCancelButton: true,
+          confirmButtonText: "GO!",
+          confirmButtonColor: "#ec6c62"
+        }, function() {
+            $scope.go('/maze/judge/'+runid+'/');
+         });
+    }
+    
+    $scope.go_approval = function(runid){
+        swal({
+          title: "Go approval page?", 
+          text: "Are you sure you want to go approval page?", 
+          type: "warning",
+          showCancelButton: true,
+          confirmButtonText: "GO!",
+          confirmButtonColor: "#ec6c62"
+        }, function() {
+            $scope.go('/admin/approval/'+runid+'/');
+         });
+    }
+    
+    $scope.go = function(path){
+      window.location = path
+  }
+    
+    
+    
+}])
+.directive("runsReadFinished", function($timeout){
+    return function(scope, element, attrs){
+      if (scope.$last){
+        $('.refine').css("visibility","visible");
+        $('.loader').remove();
+      }
+    }
 })
+
+
+$(window).on('beforeunload', function(){
+     socket.emit('unsubscribe', 'competition/' + competitionId);
+});
+
