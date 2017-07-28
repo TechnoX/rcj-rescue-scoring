5be65f1d87305611c0802e2c139e1f388d3e1d91
Sebastian Gustafsson <sebgu182@student.liu.se>
2017-07-28 12:22:14 +0900

    Don't get more data than necesairy

--- public/javascripts/line_competition.js
+++ public/javascripts/line_competition.js
@@ -5,28 +5,11 @@ angular.module("LineCompetition", []).controller("LineCompetitionController", fu
   $http.get("/api/competitions/" + competitionId +
             "/line/runs?populate=true").then(function (response) {
     $scope.runs = response.data
-    console.log($scope.teams)
+    //console.log($scope.teams)
   })
 
   $http.get("/api/competitions/" + competitionId).then(function (response) {
     $scope.competition = response.data
-  })
-
-  $http.get("/api/competitions/" + competitionId +
-            "/teams").then(function (response) {
-    $scope.teams = response.data
-  })
-  $http.get("/api/competitions/" + competitionId +
-            "/rounds").then(function (response) {
-    $scope.rounds = response.data
-  })
-  $http.get("/api/competitions/" + competitionId +
-            "/fields").then(function (response) {
-    $scope.fields = response.data
-  })
-  $http.get("/api/competitions/" + competitionId +
-            "/line/maps").then(function (response) {
-    $scope.maps = response.data
   })
   
   $scope.go = function(path){
--- public/javascripts/maze_competition.js
+++ public/javascripts/maze_competition.js
@@ -5,28 +5,11 @@ angular.module("MazeCompetition", []).controller("MazeCompetitionController", fu
   $http.get("/api/competitions/" + competitionId +
             "/maze/runs?populate=true").then(function (response) {
     $scope.runs = response.data
-    console.log($scope.teams)
+    //console.log($scope.teams)
   })
 
   $http.get("/api/competitions/" + competitionId).then(function (response) {
     $scope.competition = response.data
-  })
-
-  $http.get("/api/competitions/" + competitionId +
-            "/teams").then(function (response) {
-    $scope.teams = response.data
-  })
-  $http.get("/api/competitions/" + competitionId +
-            "/rounds").then(function (response) {
-    $scope.rounds = response.data
-  })
-  $http.get("/api/competitions/" + competitionId +
-            "/fields").then(function (response) {
-    $scope.fields = response.data
-  })
-  $http.get("/api/competitions/" + competitionId +
-            "/maze/maps").then(function (response) {
-    $scope.maps = response.data
   })
 
   $scope.go = function(path){
