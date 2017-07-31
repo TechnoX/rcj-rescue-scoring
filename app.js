9bb8eaf262a33fc94f6c7e963cc755f32e10669f
rrrobo <rrrobo@icloud.com>
2017-07-31 19:52:25 +0900

    i18n初期設定

--- app.js
+++ app.js
@@ -67,9 +67,9 @@ var app = express()
 app.use(compression())
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 /** Setting up the correct view engine - we are using jade */
 app.set('views', path.join(__dirname, 'views'))
@@ -83,7 +83,7 @@ app.set('view engine', 'jade')
 //app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
-  extended: false
+    extended: false
 }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
@@ -93,13 +93,30 @@ app.use(express.static(path.join(__dirname, 'public')))
  */
 // init passport and session
 app.use(session({
-  store : new connectMongo({
-    mongooseConnection: mongoose.connection
-  }),
-  secret: 'rcjscoring'
+    store: new connectMongo({
+        mongooseConnection: mongoose.connection
+    }),
+    secret: 'rcjscoring'
 }))
 app.use(passport.initialize())
 app.use(passport.session())
+
+
+// i18nの設定
+i18n.configure({
+    locales: ['ja', 'en'],
+    defaultLocale: 'en',
+    directory: __dirname + "/locales",
+    objectNotation: true
+});
+
+app.use(i18n.init);
+app.use(function (req, res, next) {
+    if (req.session.locale) {
+        i18n.setLocale(req, req.session.locale);
+    }
+    next();
+});
 
 
 //========================================================================
@@ -126,8 +143,8 @@ app.use('/api/competitions', [apiCompetitionsRoute.public, pass.ensureLoginApi, 
 
 app.use('/login', pass.ensureNotAuthenticated, loginRoute)
 app.use('/logout', pass.ensureAuthenticated, function (req, res, next) {
-  req.logout()
-  res.redirect('/home')
+    req.logout()
+    res.redirect('/home')
 })
 app.use('/home', homeRoute)
 
@@ -141,18 +158,18 @@ app.use('/admin', pass.ensureAdmin, adminRoute)
 
 //Simple logout (noting more neeeded)
 app.get('/logout', function (req, res) {
-  req.logout()
-  res.redirect('/')
+    req.logout()
+    res.redirect('/')
 })
 
 /*
  * This is called last in the routing config, therefor it'll take care of 404s
  */
 app.use(function (req, res, next) {
-  logger.error("404 Not Found: " + req.originalUrl)
-  var err = new Error('Not Found: ' + req.originalUrl)
-  err.status = 404
-  next(err)
+    logger.error("404 Not Found: " + req.originalUrl)
+    var err = new Error('Not Found: ' + req.originalUrl)
+    err.status = 404
+    next(err)
 })
 
 //========================================================================
@@ -161,56 +178,56 @@ app.use(function (req, res, next) {
 
 app.use(function (err, req, res, next) {
 
-  // is at base, send to login
-  if (req.originalUrl === "/") {
-    res.redirect("home")
-  }
-
-  /*
-   *  Check if it is an api or static page miss (404)
-   *
-   * One of the big things to do for scalability is to separate tcp server,static server and application server (api).
-   *
-   * Right now they are mixed and running on a single core togeather. To be able to use Node.js to 100% the  application
-   * part of the server should run alone on an own process. And the static files (CSS, HTML and Javascript(frontend)) should
-   * for performance reasons not run on Node.js but on nginx (http://nginx.org/). One of the fastest static servers out there used
-   * by many companies. Also it can work as a reverse proxy to support multi Node.js clusters.
-   *
-   */
-  else {
-
-    if (err.status != 404) {
-      logger.error(err)
+    // is at base, send to login
+    if (req.originalUrl === "/") {
+        res.redirect("home")
     }
 
+    /*
+     *  Check if it is an api or static page miss (404)
+     *
+     * One of the big things to do for scalability is to separate tcp server,static server and application server (api).
+     *
+     * Right now they are mixed and running on a single core togeather. To be able to use Node.js to 100% the  application
+     * part of the server should run alone on an own process. And the static files (CSS, HTML and Javascript(frontend)) should
+     * for performance reasons not run on Node.js but on nginx (http://nginx.org/). One of the fastest static servers out there used
+     * by many companies. Also it can work as a reverse proxy to support multi Node.js clusters.
+     *
+     */
+    else {
 
-    // since we are running api and static website on same we need to hack the different custom routes
-    var stringSplit = req.originalUrl.split("/")
-    res.status(err.status || 500)
-    if (stringSplit[1] !== undefined && stringSplit[1] === "api") {
-      res.send({
-        error: "Error 404"
-      })
-    } else {
+        if (err.status != 404) {
+            logger.error(err)
+        }
 
-      // in in development show stacktrace
-      if (app.get('env') === 'development') {
-        res.render('error', {
-          message: err.message,
-          error  : err
-        })
-      }
 
-      // in production :(
-      else {
+        // since we are running api and static website on same we need to hack the different custom routes
+        var stringSplit = req.originalUrl.split("/")
         res.status(err.status || 500)
-        res.render('error', {
-          message: err.message,
-          error  : {}
-        })
-      }
+        if (stringSplit[1] !== undefined && stringSplit[1] === "api") {
+            res.send({
+                error: "Error 404"
+            })
+        } else {
+
+            // in in development show stacktrace
+            if (app.get('env') === 'development') {
+                res.render('error', {
+                    message: err.message,
+                    error: err
+                })
+            }
+
+            // in production :(
+            else {
+                res.status(err.status || 500)
+                res.render('error', {
+                    message: err.message,
+                    error: {}
+                })
+            }
+        }
     }
-  }
 })
 
 module.exports = app
