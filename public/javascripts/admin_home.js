7316ca265ef411353d1d1828e4d244db01d359ba
rrrobo <rrrobo@icloud.com>
2017-10-03 17:14:04 +0900

    add translate setting & table change

--- public/javascripts/admin_home.js
+++ public/javascripts/admin_home.js
@@ -1,47 +1,49 @@
-angular.module("AdminHome", []).controller("AdminHomeController", function ($scope, $http) {
-  $scope.competitionId = competitionId
-  
-  updateCompetitionList()
-  
-  $scope.go = function (path) {
-    window.location = path
-  }
-  
-  $scope.addCompetition = function () {
-    var competition = {name: $scope.competitionName}
-    
-    $http.post("/api/competitions", competition).then(function (response) {
-      console.log(response)
-      updateCompetitionList()
-    }, function (error) {
-      console.log(error)
-    })
-  }
-  
-  $scope.removeCompetition = function (competition) {
-    swal({
-      title             : "Remove competition?",
-      text              : "Are you sure you want to remove the competition: " +
-                          competition.name + '?',
-      type              : "warning",
-      showCancelButton  : true,
-      confirmButtonText : "Remove it!",
-      confirmButtonColor: "#ec6c62"
-    }, function () {
-      $http.delete("/api/competitions/" +
-                   competition._id).then(function (response) {
-        console.log(response)
-        updateCompetitionList()
-      }, function (error) {
-        console.log(error)
-      })
-    });
-  }
-  
-  function updateCompetitionList() {
-    $http.get("/api/competitions/").then(function (response) {
-      $scope.competitions = response.data
-      console.log($scope.competitions)
-    })
-  }
+var app = angular.module("AdminHome", ['pascalprecht.translate', 'ngCookies']).controller("AdminHomeController", function ($scope, $http) {
+    $scope.competitionId = competitionId
+
+    updateCompetitionList()
+
+    $scope.go = function (path) {
+        window.location = path
+    }
+
+    $scope.addCompetition = function () {
+        var competition = {
+            name: $scope.competitionName
+        }
+
+        $http.post("/api/competitions", competition).then(function (response) {
+            console.log(response)
+            updateCompetitionList()
+        }, function (error) {
+            console.log(error)
+        })
+    }
+
+    $scope.removeCompetition = function (competition) {
+        swal({
+            title: "Remove competition?",
+            text: "Are you sure you want to remove the competition: " +
+                competition.name + '?',
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonText: "Remove it!",
+            confirmButtonColor: "#ec6c62"
+        }, function () {
+            $http.delete("/api/competitions/" +
+                competition._id).then(function (response) {
+                console.log(response)
+                updateCompetitionList()
+            }, function (error) {
+                console.log(error)
+            })
+        });
+    }
+
+    function updateCompetitionList() {
+        $http.get("/api/competitions/").then(function (response) {
+            $scope.competitions = response.data
+            console.log($scope.competitions)
+        })
+    }
 })
