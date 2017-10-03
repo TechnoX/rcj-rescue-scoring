ef7f5d1068aa2353c9928365de2bb81cae694972
rrrobo <rrrobo@icloud.com>
2017-10-03 17:18:18 +0900

    change locales page path

--- routes/locales.js
+++ routes/locales.js
@@ -3,10 +3,10 @@ var express = require('express')
 var router = express.Router()
 
 
-router.get('/:locale', function (req, res, next) {
-    req.session.locale = req.params.locale;
-    res.redirect('back');
-});
-
+router.get('/', function (req, res) {
+    res.render('locales', {
+        user: req.user
+    });
+})
 
 module.exports = router
