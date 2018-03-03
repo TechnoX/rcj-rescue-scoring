4420780fbdd55e8f1c8080dba243ad8650bc2a3e
rrrobo <rrrobo@icloud.com>
2018-03-03 16:21:49 +0900

    add secret command

--- public/javascripts/competition_home.js
+++ public/javascripts/competition_home.js
@@ -1,6 +1,6 @@
 var app = angular.module("CompetitionHome", ['pascalprecht.translate', 'ngCookies']);
 app.controller("CompetitionHomeController", ['$scope', '$http', '$translate', function ($scope, $http, $translate) {
-    
+    $scope.secretCommand = false;
     $http.get("/api/competitions/" + competitionId).then(function (response) {
         $scope.competition = response.data
     })
@@ -12,5 +12,10 @@ app.controller("CompetitionHomeController", ['$scope', '$http', '$translate', fu
     $scope.go = function (path) {
         window.location = path
     }
+    
+    cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () {
+        $scope.secretCommand = !$scope.secretCommand;
+        $scope.$apply();
+    });
 
 }]);
