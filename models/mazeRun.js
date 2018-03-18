3f047b277b9f475ff6a2897fb9b43829b35e4b4d
rrrobo <rrrobo@icloud.com>
2018-03-18 22:31:34 +0900

    allow no team & add group

--- models/mazeRun.js
+++ models/mazeRun.js
@@ -28,9 +28,10 @@ const mazeRunSchema = new Schema({
     index   : true
   },
   round      : {type: ObjectId, ref: 'Round', required: true, index: true},
-  team       : {type: ObjectId, ref: 'Team', required: true, index: true},
+  team       : {type: ObjectId, ref: 'Team', required: false, index: true},
   field      : {type: ObjectId, ref: 'Field', required: true, index: true},
   map        : {type: ObjectId, ref: 'MazeMap', required: true, index: true},
+  group      : {type: Number, min: 0},
   
   tiles    : [{
     x          : {type: Number, integer: true, required: true},
@@ -87,113 +88,190 @@ mazeRunSchema.pre('save', function (next) {
     } else {
       
       if (self.isNew) {
-        MazeRun.findOne({
-          round: self.round,
-          team : self.team
-        }).populate("round team").exec(function (err, dbRun) {
-          if (err) {
-            return next(err)
-          } else if (dbRun) {
-            err = new Error('Team "' + dbRun.team.name +
-                            '" already has a run in round "' +
-                            dbRun.round.name +
-                            '"!')
-            return next(err)
-          } else {
-            // Check that all references matches
+        if (self.team){
+            MazeRun.findOne({
+              round: self.round,
+              team : self.team
+            }).populate("round team").exec(function (err, dbRun) {
+              if (err) {
+                return next(err)
+              } else if (dbRun) {
+                err = new Error('Team "' + dbRun.team.name +
+                                '" already has a run in round "' +
+                                dbRun.round.name +
+                                '"!')
+                return next(err)
+              } else {
+                // Check that all references matches
+                async.parallel({
+                    competition: function (callback) {
+                      competitiondb.competition.findById(self.competition, function (err, dbCompetition) {
+                        if (err) {
+                          return callback(err)
+                        } else if (!dbCompetition) {
+                          return callback(new Error("No competition with that id!"))
+                        } else {
+                          return callback(null, dbCompetition)
+                        }
+                      })
+                    },
+                    round      : function (callback) {
+                      competitiondb.round.findById(self.round, function (err, dbRound) {
+                        if (err) {
+                          return callback(err)
+                        } else if (!dbRound) {
+                          return callback(new Error("No round with that id!"))
+                        } else {
+                          return callback(null, dbRound)
+                        }
+                      })
+                    },
+                    team       : function (callback) {
+                      competitiondb.team.findById(self.team, function (err, dbTeam) {
+                        if (err) {
+                          return callback(err)
+                        } else if (!dbTeam) {
+                          return callback(new Error("No team with that id!"))
+                        } else {
+                          return callback(null, dbTeam)
+                        }
+                      })
+                    },
+                    field      : function (callback) {
+                      competitiondb.field.findById(self.field, function (err, dbField) {
+                        if (err) {
+                          return callback(err)
+                        } else if (!dbField) {
+                          return callback(new Error("No field with that id!"))
+                        } else {
+                          return callback(null, dbField)
+                        }
+                      })
+                    },
+                    map        : function (callback) {
+                      mazeMapdb.mazeMap.findById(self.map, function (err, dbMap) {
+                        if (err) {
+                          return callback(err)
+                        } else if (!dbMap) {
+                          return callback(new Error("No map with that id!"))
+                        } else {
+                          return callback(null, dbMap)
+                        }
+                      })
+                    }
+                  },
+                  function (err, results) {
+                    if (err) {
+                      return next(err)
+                    } else {
+                      const competitionId = results.competition.id
+
+                      if (results.round.competition != competitionId) {
+                        return next(new Error("Round does not match competition!"))
+                      }
+                      if (MAZE_LEAGUES.indexOf(results.round.league) == -1) {
+                        return next(new Error("Round does not match league!"))
+                      }
+
+                      if (results.team.competition != competitionId) {
+                        return next(new Error("Team does not match competition!"))
+                      }
+                      if (MAZE_LEAGUES.indexOf(results.team.league) == -1) {
+                        return next(new Error("Team does not match league!"))
+                      }
+
+                      if (results.field.competition != competitionId) {
+                        return next(new Error("Field does not match competition!"))
+                      }
+                      if (MAZE_LEAGUES.indexOf(results.field.league) == -1) {
+                        return next(new Error("Field does not match league!"))
+                      }
+
+                      if (results.map.competition != competitionId) {
+                        return next(new Error("Map does not match competition!"))
+                      }
+
+                      return next()
+                    }
+                  })
+              }
+            })
+        }
+        else{
             async.parallel({
-                competition: function (callback) {
-                  competitiondb.competition.findById(self.competition, function (err, dbCompetition) {
+                    competition: function (callback) {
+                      competitiondb.competition.findById(self.competition, function (err, dbCompetition) {
+                        if (err) {
+                          return callback(err)
+                        } else if (!dbCompetition) {
+                          return callback(new Error("No competition with that id!"))
+                        } else {
+                          return callback(null, dbCompetition)
+                        }
+                      })
+                    },
+                    round      : function (callback) {
+                      competitiondb.round.findById(self.round, function (err, dbRound) {
+                        if (err) {
+                          return callback(err)
+                        } else if (!dbRound) {
+                          return callback(new Error("No round with that id!"))
+                        } else {
+                          return callback(null, dbRound)
+                        }
+                      })
+                    },
+                    field      : function (callback) {
+                      competitiondb.field.findById(self.field, function (err, dbField) {
+                        if (err) {
+                          return callback(err)
+                        } else if (!dbField) {
+                          return callback(new Error("No field with that id!"))
+                        } else {
+                          return callback(null, dbField)
+                        }
+                      })
+                    },
+                    map        : function (callback) {
+                      mazeMapdb.mazeMap.findById(self.map, function (err, dbMap) {
+                        if (err) {
+                          return callback(err)
+                        } else if (!dbMap) {
+                          return callback(new Error("No map with that id!"))
+                        } else {
+                          return callback(null, dbMap)
+                        }
+                      })
+                    }
+                  },
+                  function (err, results) {
                     if (err) {
-                      return callback(err)
-                    } else if (!dbCompetition) {
-                      return callback(new Error("No competition with that id!"))
+                      return next(err)
                     } else {
-                      return callback(null, dbCompetition)
+                      const competitionId = results.competition.id
+
+                      if (results.round.competition != competitionId) {
+                        return next(new Error("Round does not match competition!"))
+                      }
+                      if (MAZE_LEAGUES.indexOf(results.round.league) == -1) {
+                        return next(new Error("Round does not match league!"))
+                      }
+
+                      if (results.field.competition != competitionId) {
+                        return next(new Error("Field does not match competition!"))
+                      }
+                      if (MAZE_LEAGUES.indexOf(results.field.league) == -1) {
+                        return next(new Error("Field does not match league!"))
+                      }
+
+                      if (results.map.competition != competitionId) {
+                        return next(new Error("Map does not match competition!"))
+                      }
+
+                      return next()
                     }
                   })
-                },
-                round      : function (callback) {
-                  competitiondb.round.findById(self.round, function (err, dbRound) {
-                    if (err) {
-                      return callback(err)
-                    } else if (!dbRound) {
-                      return callback(new Error("No round with that id!"))
-                    } else {
-                      return callback(null, dbRound)
-                    }
-                  })
-                },
-                team       : function (callback) {
-                  competitiondb.team.findById(self.team, function (err, dbTeam) {
-                    if (err) {
-                      return callback(err)
-                    } else if (!dbTeam) {
-                      return callback(new Error("No team with that id!"))
-                    } else {
-                      return callback(null, dbTeam)
-                    }
-                  })
-                },
-                field      : function (callback) {
-                  competitiondb.field.findById(self.field, function (err, dbField) {
-                    if (err) {
-                      return callback(err)
-                    } else if (!dbField) {
-                      return callback(new Error("No field with that id!"))
-                    } else {
-                      return callback(null, dbField)
-                    }
-                  })
-                },
-                map        : function (callback) {
-                  mazeMapdb.mazeMap.findById(self.map, function (err, dbMap) {
-                    if (err) {
-                      return callback(err)
-                    } else if (!dbMap) {
-                      return callback(new Error("No map with that id!"))
-                    } else {
-                      return callback(null, dbMap)
-                    }
-                  })
-                }
-              },
-              function (err, results) {
-                if (err) {
-                  return next(err)
-                } else {
-                  const competitionId = results.competition.id
-                  
-                  if (results.round.competition != competitionId) {
-                    return next(new Error("Round does not match competition!"))
-                  }
-                  if (MAZE_LEAGUES.indexOf(results.round.league) == -1) {
-                    return next(new Error("Round does not match league!"))
-                  }
-                  
-                  if (results.team.competition != competitionId) {
-                    return next(new Error("Team does not match competition!"))
-                  }
-                  if (MAZE_LEAGUES.indexOf(results.team.league) == -1) {
-                    return next(new Error("Team does not match league!"))
-                  }
-                  
-                  if (results.field.competition != competitionId) {
-                    return next(new Error("Field does not match competition!"))
-                  }
-                  if (MAZE_LEAGUES.indexOf(results.field.league) == -1) {
-                    return next(new Error("Field does not match league!"))
-                  }
-                  
-                  if (results.map.competition != competitionId) {
-                    return next(new Error("Map does not match competition!"))
-                  }
-                  
-                  return next()
-                }
-              })
-          }
-        })
+        }
       } else {
         return next()
       }
