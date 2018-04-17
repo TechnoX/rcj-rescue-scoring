fb6b1d9679cfe20d5a0aef2bb6ab13f41686be95
Sebastian Gustafsson <sebgu182@student.liu.se>
2018-04-17 17:54:55 +0200

    Frontend

--- models/line.map.model.js
+++ models/line.map.model.js
@@ -2,7 +2,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const ObjectId = Schema.Types.ObjectId
-const async = require('async')
 
 const logger = require('../config/logger').mainLogger
 
