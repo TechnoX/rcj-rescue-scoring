e98ae986fb1a12cb80926c9ac95ad7b0a041e18d
Jan Blumenkamp <ja_bl@uni-bremen.de>
2018-05-27 00:49:16 +0200

    made positionmakrer processing completely universal, refactored to processPosMarkers

--- helper/scoreSheetLineProcess.js
+++ helper/scoreSheetLineProcess.js
@@ -10,15 +10,14 @@ function findPosdataByDescr(data, descriptor) {
 function drawPosdataToSheet(sheetMat, posData) {
   for (let i = 0; i < posData.data.length; i++) {
     let man = posData.data[i].posData;
-    sheetMat.drawRectangle(new cv.Rect(man.x - 40, man.y - 80, man.w, man.h),
+    sheetMat.drawRectangle(new cv.Rect(man.x, man.y, man.w, man.h),
       new cv.Vec3(0, 255, 0), 2, 4, 0);
   }
-
 
   for (let i = 0; i < posData.tiles.length; i++) {
     for (let j = 0; j < posData.tiles[i].length; j++) {
       let man = posData.tiles[i][j].posData;
-      sheetMat.drawRectangle(new cv.Rect(man.x - 40, man.y - 80, man.w, man.h),
+      sheetMat.drawRectangle(new cv.Rect(man.x, man.y, man.w, man.h),
         new cv.Vec3(0, 255, 0), 2, 4, 0);
     }
   }
@@ -39,7 +38,7 @@ function drawPosdataToSheet(sheetMat, posData) {
  *    QR: null if no QR code could be identified, otherwise the data of the code as string
  */
 function processPosdataElement(mat, posdata) {
-  let matPosdata = mat.getRegion(new cv.Rect(posdata.x - 40, posdata.y - 80, posdata.w, posdata.h));
+  let matPosdata = mat.getRegion(new cv.Rect(posdata.x, posdata.y, posdata.w, posdata.h));
 
   switch (posdata.type) {
     case InputTypeEnum.POSMARK:
@@ -90,11 +89,19 @@ function processPosdataElement(mat, posdata) {
   }
 }
 
-module.exports.processScoreSheet = function(posData, config) {
+/**
+ * Extracts position markers from the raw input image and scales the image in a way that
+ * the posData pixel value from the sheet generation match the image
+ * @param sheetMat raw sheet image
+ * @param config configuration object from sheet generation
+ * @param posMarkersPosData posData
+ * @returns {Mat} normalized sheet
+ */
+function processPosMarkers(sheetMat, config, posMarkersPosData) {
   const params = new cv.SimpleBlobDetectorParams();
   params.filterByArea = false;
   params.filterByCircularity = true;
-  params.minCircularity = 0.7; // Squares
+  params.minCircularity = 0.7; // Find Squares
   params.minCircularity = 0.8;
   params.filterByColor = false;
   params.filterByConvexity = false;
@@ -104,24 +111,36 @@ module.exports.processScoreSheet = function(posData, config) {
   params.maxThreshold = 200;
 
   const detector = new cv.SimpleBlobDetector(params);
-  const sheet = cv.imread('helper/scoresheet_new.png').bgrToGray();
-  const allKeypoints = detector.detect(sheet.gaussianBlur(new cv.Size(5, 5), 0, 0, cv.BORDER_CONSTANT));
+  const allKeypoints = detector.detect(sheetMat.gaussianBlur(new cv.Size(5, 5), 0, 0, cv.BORDER_CONSTANT));
   const largestKeypoints = allKeypoints.sort((k1, k2) => k2.size - k1.size).slice(0, 3);
-
-  const posMarkers = findPosdataByDescr(posData.data, 'posMarkers');
 
   const keyPointsSortX = largestKeypoints.slice(0).sort((k1, k2) => k2.point.x - k1.point.x);
   const keyPointsSortY = largestKeypoints.slice(0).sort((k1, k2) => k2.point.y - k1.point.y);
 
-  const sheetProc = sheet.getRegion(
+  return sheetMat.getRegion(
     new cv.Rect(
       keyPointsSortX[2].point.x,
       keyPointsSortY[2].point.y,
       keyPointsSortX[0].point.x - keyPointsSortX[2].point.x,
       keyPointsSortY[0].point.y - keyPointsSortY[2].point.y
     )
-  ).resizeToMax(Math.max(posMarkers.w, posMarkers.h));
+  ).resizeToMax(posMarkersPosData.h)
+    .warpAffine(
+      new cv.Mat([
+          [1, 0, config.positionMarkers[0].x + config.positionMarkersSize / 2],
+          [0, 1, config.positionMarkers[0].y + config.positionMarkersSize / 2]
+        ], cv.CV_32FC1
+      ), new cv.Size(posMarkersPosData.w + config.positionMarkers[0].x, posMarkersPosData.h + config.positionMarkers[0].y)
+    );
+}
 
-  console.log(processPosdataElement(sheetProc, findPosdataByDescr(posData.data, 'meta')));
-  console.log(processPosdataElement(sheetProc, findPosdataByDescr(posData.data, 'enterManually')))
+module.exports.processScoreSheet = function(posData, config) {
+  const normalizedSheet = processPosMarkers(cv.imread('helper/scoresheet_n.png').bgrToGray(), config, findPosdataByDescr(posData.data, 'posMarkers'));
+
+  //console.log(processPosdataElement(sheetProc, findPosdataByDescr(posData.data, 'meta')));
+  //console.log(processPosdataElement(sheetProc, findPosdataByDescr(posData.data, 'enterManually')))
+  //console.log(processTileData(sheetProc, posData.tiles))
+
+  drawPosdataToSheet(normalizedSheet, posData);
+  cv.imwrite("helper/out.png", normalizedSheet)
 };
