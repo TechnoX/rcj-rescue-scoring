54ca3bd93704eb422d318e628d7f153220bf70f9
rrrobo <rrrobo@icloud.com>
2018-06-02 00:46:29 +0900

    workbox config for debug

--- workbox-config.js
+++ workbox-config.js
@@ -1,13 +1,24 @@
 module.exports = {
-  "globDirectory": "public/",
-  "globPatterns": [
-    "javascripts/*.js",
-    "lang/*.json",
-    "stylesheets/*.css",
-    "templates/*.html",
+    "globDirectory": "public/",
+    "globPatterns": [
+    //"javascripts/*.js",
+    //"lang/*.json",
+    //"stylesheets/*.css",
+    //"templates/*.html",
     "sounds/*.mp3",
     "images/**/*"
   ],
-  "swDest": "public/sw.js",
-  importWorkboxFrom: 'local'
+    "swDest": "public/sw.js",
+    importWorkboxFrom: 'local',
+
+    // Define runtime caching rules.
+    runtimeCaching: [
+        {
+            // Match any request ends with .png, .jpg, .jpeg or .svg.
+            urlPattern: /\.(?:js|css|scss|woff2)$/,
+
+            // Apply a cache-first strategy.
+            handler: 'cacheFirst',
+    },
+  ],
 };
